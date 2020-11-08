d1401f20439fab9935bb02ab3b1ec9b0553353c2
Nastya <riumina20133@mail.ru>
2020-11-08 20:57:02 +0300

    The program reads the file line by line. On a line> 5 characters, then this line is placed in the database table

--- src/com/company/ReadFileLineByLine.java
+++ src/com/company/ReadFileLineByLine.java
@@ -1,39 +1,54 @@
 package com.company;
 
 import java.io.*;
-// построчно читаем файл файла
+import java.sql.*;
+
+
+// построчно читаем файл файла и выводим его в таблицу БД
 public class ReadFileLineByLine {
 
     public static void main(String[] args) {
         try {
             File file = new File("D:\\Text.txt.txt");
-            //создаем объект FileReader для объекта File
+
             FileReader fr = new FileReader(file);
-            //создаем BufferedReader с существующего FileReader для построчного считывания
             BufferedReader reader = new BufferedReader(fr);
-            // считываем первую строку
-            String str = reader.readLine();
-            // пока строка не пустая,проделываем махинацию:
-            while (str != null) {
-                // вводим целочисленную переменную length,которая подсчитывает символы в строке,
-                int length = str.length();
-                // если в строке больше 5 символов - выводим эту строчку
-                if (length>5){
-                    System.out.println(str);
+
+            String stra = reader.readLine();
+
+            while (stra != null) {
+
+                int length = stra.length();
+                if (length > 5) {
+
+                    final String url = "jdbc:mysql://localhost:3306/employees?serverTimezone=UTC";
+                    final String user = "root";
+                    final String password = "root";
+
+                        try {
+                            Connection connection = DriverManager.getConnection(url, user, password);
+                            String sql = "INSERT INTO workers (The_Words)  VALUES (?)";
+                            PreparedStatement stat = connection.prepareStatement(sql);
+                            stat.setString(1, stra);
+                            stat.executeUpdate();
+                            connection.close();
+
+                         }catch (SQLException e) {
+                            e.printStackTrace();
+                        }
                 }
-                // считываем остальные строки в цикле
-                str = reader.readLine();
+                stra = reader.readLine();
             }
-        } 
+        }
         //Исключения
-        catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
+        catch (IOException e) {
             e.printStackTrace();
         }
     }
-
 }
+
+
+
 
 
 
