38153c65aa059ca596b64c3c7cb6342924c36481
Nastya <riumina20133@mail.ru>
2020-11-09 16:45:27 +0300

    The program reads the file line by line. On a string> 5 characters, a method from another class is called that adds the string to the database

--- src/com/company/ReadFileLineByLine.java
+++ src/com/company/ReadFileLineByLine.java
@@ -1,51 +1,37 @@
 package com.company;
 
 import java.io.*;
-import java.sql.*;
+//import java.sql.*;
 
-
-// построчно читаем файл файла и выводим его в таблицу БД
-public class ReadFileLineByLine {
+ class ReadFileLineByLine {
+     static String stra;
 
     public static void main(String[] args) {
+
+
         try {
             File file = new File("D:\\Text.txt.txt");
 
             FileReader fr = new FileReader(file);
             BufferedReader reader = new BufferedReader(fr);
+            SendToTheDatabase dataB = new SendToTheDatabase();
 
-            String stra = reader.readLine();
+             stra = reader.readLine();
 
             while (stra != null) {
-
                 int length = stra.length();
+
                 if (length > 5) {
-
-                    final String url = "jdbc:mysql://localhost:3306/employees?serverTimezone=UTC";
-                    final String user = "root";
-                    final String password = "root";
-
-                        try {
-                            Connection connection = DriverManager.getConnection(url, user, password);
-                            String sql = "INSERT INTO workers (The_Words)  VALUES (?)";
-                            PreparedStatement stat = connection.prepareStatement(sql);
-                            stat.setString(1, stra);
-                            stat.executeUpdate();
-                            connection.close();
-
-                         }catch (SQLException e) {
-                            e.printStackTrace();
-                        }
+                    dataB.send();
                 }
                 stra = reader.readLine();
             }
         }
-        //Исключения
         catch (IOException e) {
             e.printStackTrace();
         }
     }
-}
+    }
 
 
 
